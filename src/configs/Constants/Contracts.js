5ac555da3f26e2e985a7cddcdc35cd6389c7cd0d
Dong Liu <winter.solstice1219@gmail.com>
2022-02-08 19:36:27 -0500

    Update the project

--- src/configs/Constants/Contracts.js
+++ src/configs/Constants/Contracts.js
@@ -4,9 +4,9 @@ const { ETH_MAINNET, ETH_TESTNET } = CHAIN_ID
 
 export const contractAddresses = {
   [ETH_MAINNET]: {
-    address: "0xa9a8317f1B8E24ef6E9d558085Afd2081D3d25E6",
+    address: "0x2FCf0ec9D6b575838aB77E6f567DA143A8560d6d",
     explorerUrl:
-      "https://rinkeby.etherscan.io/token/0xa9a8317f1B8E24ef6E9d558085Afd2081D3d25E6",
+      "https://etherscan.io/token/0x2fcf0ec9d6b575838ab77e6f567da143a8560d6d",
   },
   [ETH_TESTNET]: {
     address: "0x30C4Df9E5323B1844bA91573B4966E7Af9A93683",
